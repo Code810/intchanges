3da9c10b8c726ed28e36f71da05ca15730b8926a
code810 <nadirssh@code.edu.az>
2023-10-08 00:08:42 +0400

    second

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,8 @@
 ﻿int num1 = 34;
 int num2 = 5;
-num1 = num1 + num2;
-num2 = num1 - num2;
-num1 = num1 - num2;
-
+//num1 = num1 + num2;
+//num2 = num1 - num2;
+//num1 = num1 - num2;
+(num1, num2) = (num2, num1);
 Console.WriteLine(num1);
 Console.WriteLine(num2);
