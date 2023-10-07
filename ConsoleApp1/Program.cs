af6d2748c4f09daee7c78a0e27d71873ae35f3ae
code810 <nadirssh@code.edu.az>
2023-10-08 00:46:02 +0400

    testvisual

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,32 @@
-﻿int num1 = 34;
-int num2 = 5;
+﻿#region
+//int num1 = 34;
+//int num2 = 5;
+
 //num1 = num1 + num2;
 //num2 = num1 - num2;
 //num1 = num1 - num2;
-(num1, num2) = (num2, num1);
-Console.WriteLine(num1);
-Console.WriteLine(num2);
+
+////(num1, num2) = (num2, num1); 
+
+//Console.WriteLine(num1);
+//Console.WriteLine(num2);
+#endregion
+#region
+int num1 = 90;
+int num2 = 95;
+
+if (num1 == num2) 
+{
+    Console.WriteLine(num1 + num2);
+}
+
+else if (num2 > num1)
+{
+    Console.WriteLine(num2);
+}
+//else (num2 < num1)
+//{
+//    Console.WriteLine (num1);
+//}
+#endregion
+
